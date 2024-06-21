7e9667e68afc32e5c12e35e30c3275d3d01762a5
ssonar01 <109798715+ssonar01@users.noreply.github.com>
2024-06-22 02:59:17 +0530

    Updated function for proper loading
    changes made to the function to load the libraries in namespace and updated test file to reflect loadedNameSpaces

--- R/libraryloader.R
+++ R/libraryloader.R
@@ -1,16 +1,14 @@
 #' Prepare libraries for loading
 #'
-#' @param x A vector of package names necessary for functional loading
+#' @param pkgs A vector of package names necessary for functional loading
 #' @export
 #' @import glue
 #' @importFrom utils install.packages installed.packages
-required_package <- function(x){
-  if (!all(x %in% utils::installed.packages()[,'Package'])){
-    print('Installing Required Packages . . .')
-    lapply(x,utils::install.packages,character.only = TRUE)
-    print('Required Packages installed')
-  }else{
-    lapply(x,library,character.only = TRUE)
-    print(glue::glue("Package {x} is already installed and loaded!"))
-  }
+required_package <- function(pkgs){
+  lapply(pkgs, function(pkg){
+    if(!requireNamespace(pkg, quietly = TRUE)){
+      utils::install.packages(pkg,repos = "https://cloud.r-project.org")
+    }
+    library(pkg, character.only = TRUE)
+  })
 }
--- tests/testthat/test_required_package.R
+++ tests/testthat/test_required_package.R
@@ -1,11 +1,12 @@
 library(testthat)
 library(libraryloader)
 
-options(repos = "https://cloud.r-project.org")
+# options(repos = "https://cloud.r-project.org")
 
 test_that("Test required_package function loading libraries correctly", {
-  pkg_list <- c("data.table", "ggplot2")
+  pkg_list <- c("stats", "ggplot2")
   required_package(pkg_list)
+  print(sessionInfo())
   expect_true(all(pkg_list %in% loadedNamespaces()),
               "All packages should be loaded after calling required_package")
 })
